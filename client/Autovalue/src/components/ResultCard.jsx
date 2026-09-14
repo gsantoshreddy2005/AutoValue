be7b7943855d5a6e0e809a99e100d2434daa5152
gsantoshreddy2005 <gsantoshreddy2005@gmail.com>
2026-09-14 18:05:15 +0530

    Complete AutoValue frontend functionality

---
+++ client/Autovalue/src/components/ResultCard.jsx
@@ -0,0 +1,15 @@
+import "./ResultCard/css"
+
+export default function ResultCard({prediction}) {
+    return (
+        <div className="result-card">
+            <p className="result-label">Estimated resale Value</p>
+
+            <h2>${prediction}</h2>
+
+            <p className="result-message">
+                This is the estimated value of your car based on the details provided.
+            </p>
+        </div>
+    );
+}
